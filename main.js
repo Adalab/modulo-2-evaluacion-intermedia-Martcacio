74d5c457e1f9a3360fcc31e358f359c22d1fc9ab
Marta Cano <martanchor@gmail.com>
2021-01-28 10:42:41 +0100

    update all

---
+++ main.js
@@ -0,0 +1,35 @@
+'use strict';
+const updateElement = document.querySelector('.js-button');
+const choosenNumber = document.querySelector('.js-action');
+const numberTip = document.querySelector('.js-tip');
+const numberTry = document.querySelector('.js-try');
+
+
+const randomNumber = Math.random();
+const randomNumberHundred = randomNumber * 100;
+const randomNumberSimple = Math.ceil(randomNumberHundred);
+console.log('El número aleatorio es:',
+    randomNumberSimple);
+
+
+
+function guessNumber() {
+
+    let choosenNumberValue = choosenNumber.value;
+    choosenNumberValue = parseInt(choosenNumberValue);
+
+    if (choosenNumberValue < randomNumberSimple) {
+        numberTip.value = 'Demasiado bajo';
+    } else if (choosenNumberValue > randomNumberSimple) {
+        numberTip.value = 'Demasiado alto';
+    } else if (choosenNumberValue = randomNumberSimple) {
+        numberTip.value = 'Has ganado campeona!!';
+    } else { numberTip.value = 'El número debe estar entre el 1 y el 100' }
+
+    contador = 0 + 'click';
+
+}
+updateElement.addEventListener('click', guessNumber);
+
+//Contador
+let contador = 0;
